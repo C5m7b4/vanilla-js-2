6297389ff8d7622f5112db32b37f4b55cc567ff9
C5m7b4 <mbedingfield@dcrpos.com>
2022-09-29 11:14:11 -0500

    add logic and styles for table

--- src/index.js
+++ src/index.js
@@ -45,3 +45,16 @@ const inputs = document.getElementsByTagName("input");
 for (let input of inputs) {
   input.addEventListener("change", changeState);
 }
+
+const buildTable = () => {
+  let html = `<table style="width:90%; margin: 20px auto; color: #000">`;
+  html +=
+    "<tr><th>Products</th><th>Size</th><th>Price</th><th>Category</th><th>Delete</th></tr>";
+  filteredData.map((item) => {
+    const { name, id, price, category, size } = item;
+    html += `<tr><td>${name}</td><td>${size}</td><td>${price}</td><td>${category}</td><td style="cursor:pointer;" onClick="deleteItem(${id})">Delete</td></tr>`;
+  });
+  html += "</table>";
+  document.getElementById("items").innerHTML = html;
+};
+buildTable();
