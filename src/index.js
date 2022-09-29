4ab9693784be02fb9247ad423a059838f0232d8d
C5m7b4 <mbedingfield@dcrpos.com>
2022-09-29 10:41:52 -0500

    add data and stub out the app

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
 console.log("you are ready to code bro");
+import { data } from "./data";
+import { isValid } from "./utils";
 import "./styles.css";
+
+let filteredData = data;
+
+const state = {
+  items: data,
+  currentItem: {
+    name: "",
+    size: "",
+    price: 0,
+    category: "",
+  },
+};
+
+console.log(filteredData);
+console.log(state);
+const someVar = "Hello";
+console.log(isValid(someVar));
+console.log(isValid(null));
